da762027bbc35fc8882b94b4a3ad20f6f41f539c
Žiga Pregelj <zpregelj@gmail.com>
2025-12-12 20:56:33 +0100

    day 6 part 2 refactor and inline comments

--- Day 06/main.py
+++ Day 06/main.py
@@ -20,6 +20,7 @@ def multiply_vertical_list(vertical_list: list[int]):
         multiplication *= number
     return multiplication
 
+
 # put columns into one list
 def part_1(file_name):
     grand_total = 0
@@ -37,23 +38,12 @@ def part_1(file_name):
     return grand_total
 
 
-def part_2(file_name):
-    ...
-
-
 def test_part_1():
     assert part_1(s) == 4277556
     assert part_1(l) == 4719804927602
 
 
-def test_part_2():
-    assert part_2(s) == None
-
-
 if __name__ == "__main__":
     small_input = read_lines(s)
     large_input = read_lines(l)
-    print(small_input)
-    print(large_input)
     print("First part: ", part_1(l))
-    print("Second part: ", part_2(l))
--- Day 06/main_2.py
+++ Day 06/main_2.py
@@ -4,20 +4,22 @@ from sys import exc_info
 s = "small_input.txt"
 l = "input.txt"
 
-# new version
+
+# new version -  get just raw strings of numbers and operations separately
 def read_lines(file_name: str) -> tuple[list[str], list[int]]:
     with open(file_name, "r", encoding="utf-8") as file:
         lines_to_list = [line.rstrip("\n") for line in file]
-
         operations = [operation for operation in lines_to_list[-1].strip().split(" ") if operation != ""]
         numbers_in_columns = []
         for column in lines_to_list[:-1]:
-            new_column_to_int = [number for number in column]
             numbers_in_columns.append(column)
         return operations, numbers_in_columns
+
+
 print(read_lines(s))
 
 
+# for each column we need to know how big is particular number (how many places (char) it takes)
 def count_length_of_biggest_number_in_column(file_name: str):
     with open(file_name, "r", encoding="utf-8") as file:
         lines_to_list = [line for line in file]
@@ -26,6 +28,7 @@ def count_length_of_biggest_number_in_column(file_name: str):
         # take only last line with operations and reverse it (because is easier to count spaces)
         for operation in reversed(lines_to_list[-1]):
             if operation == "*" or operation == "+":
+                # detect on which index there is operator
                 numbers_length_in_column.append(count_spaces)
                 count_spaces = 0
             else:
@@ -34,44 +37,39 @@ def count_length_of_biggest_number_in_column(file_name: str):
         return numbers_length_in_column[::-1]
 
 
-print(count_length_of_biggest_number_in_column(l))
-print(count_length_of_biggest_number_in_column(s))
-
-
 def cut_string_into_numbers_with_spaces(file_name):
     operations, numbers_in_columns = read_lines(file_name)
     cell_length = count_length_of_biggest_number_in_column(file_name)
     count_numbers_in_row = len(operations)
     rearranged_numbers = []
     for line in numbers_in_columns:
-        new_column =  []
+        new_column = []
         main_index = 0
         # count also " " space between numbers
-        # new_column.append(line[start_index:end_index])
 
         for i in range(count_numbers_in_row - 1):
-            # print(length)
+            # get length of cell into for each index to know how much we need to move forward in slices
             length = cell_length[i]
-
-            # index_to_add += 1
-            # start_index += index_to_add
-            # end_index += index_to_add
+            # slice line to get only number with relevant " " spaces
             new_column.append(line[main_index:main_index + length])
+            # add one to skip inside spaces " " remove inside spaces
             main_index = main_index + length + 1
-        
-        # add last number in column
+
+        # add last number in column (get last number length from cell_length function)
         new_column.append(line[main_index:main_index + cell_length[-1]])
         rearranged_numbers.append(new_column)
         new_column = []
-        start_index = 0
     return rearranged_numbers
-        
+
+
 print(cut_string_into_numbers_with_spaces(l))
 
+
+# put rows into columns, we need to get numbers from top to down in the same column nad in the same cell from top down
 def remap_numbers_in_right_list(file_name):
     numbers_in_list = cut_string_into_numbers_with_spaces(file_name)
     new_order_list = []
-    
+
     for i in range(len(numbers_in_list[0])):
         rearranged_list = []
         for j in range(len(numbers_in_list)):
@@ -81,55 +79,31 @@ def remap_numbers_in_right_list(file_name):
 
     return new_order_list
 
+
 print("this put rows to columns", remap_numbers_in_right_list(s))
 
-def index_of_spaces(file_name: str):
-    with open(file_name, "r", encoding="utf-8") as file:
-        lines_to_list = [line.strip() for line in file]
-        numbers_in_row = len([operation for operation in lines_to_list[-1].strip().split(" ") if operation != ""])
-        i = 0
-        space_index = set()
-        for column in lines_to_list[:-1]:
-            for number in column.strip().split(" "):
-                if number != "":
-                    i += 1
-                else:
-                    space_index.add(i % numbers_in_row)
-        return sorted(space_index)
 
-   
-    
-def not_split(file_name: str):
-    with open(file_name, "r", encoding="utf-8") as file:
-        lines_to_list = [line for line in file]
-        numbers_length_in_column = count_length_of_biggest_number_in_column(file_name)
-        i = 0
-        # print(lines_to_list[-1])
-        for column in lines_to_list[:-1]:
-            for colum_length in numbers_length_in_column:
-                start_index = 0
-                end_index = colum_length
-                # print(column[:3])
-                ...
-
+# put rows into columns (change order in lists)
 def transform_rows_to_columns(list_of_one_column):
+    # get len of the biggest number
     list_max_number_size = len(max(list_of_one_column))
     new_transformed_list = []
-    # how to detect on which side is " "
+    # j go over range of the biggest number
     for j in range(list_max_number_size):
-
         new_number = ""
-        for i, number in enumerate(list_of_one_column):
+        # in the second loop go over a list
+        for number in list_of_one_column:
             new_number += number[j]
         new_transformed_list.append(new_number)
         new_number = ""
 
     return new_transformed_list
 
-print(transform_rows_to_columns(['123', ' 45', '  6']))
-print(transform_rows_to_columns(['328', '64 ', '98 ']))
-print(transform_rows_to_columns([' 51', '387', '215']))
 
+print(transform_rows_to_columns(['123', ' 45', '  6']))
+
+
+# cut spaces from numbers and convert string into int and put in the same list (the same structure)
 def transform_string_numbers_to_int(list_of_column):
     int_column = []
     for string_number in list_of_column:
@@ -138,33 +112,28 @@ def transform_string_numbers_to_int(list_of_column):
         int_column.append(number)
     return int_column
 
-print(transform_string_numbers_to_int(['1  ', '24 ', '356']))
+
 print(transform_string_numbers_to_int(transform_rows_to_columns(['123', ' 45', '  6'])))
 
+
+# helper function for multiplication in a list
 def multiply_vertical_list(vertical_list: list[int]):
     multiplication = 1
     for number in vertical_list:
         multiplication *= number
     return multiplication
 
-"""
-64
-23
-314
-----
-read from right to left each digit is own column (read column from top to 
-4 + 431 + 623 = 1058
-"""
 
 def part_2(file_name):
     grand_total = 0
+    # transformed data already in 
     new_order_column_to_row = remap_numbers_in_right_list(file_name)
     operations, _ = read_lines(file_name)
 
     for i, column in enumerate(new_order_column_to_row):
         # multiplication or sum
         operation = operations[i]
-        
+        # transform and cut spaces
         ints_in_column_in_right_order = transform_string_numbers_to_int(transform_rows_to_columns(column))
 
         if operation == "*":
@@ -174,12 +143,11 @@ def part_2(file_name):
 
     return grand_total
 
-print(part_2(s))
-
-
 
 def test_part_2():
     assert part_2(s) == 3263827
+    assert part_2(l) == 9608327000261
+
 
 # too high
 # 9675710241167
