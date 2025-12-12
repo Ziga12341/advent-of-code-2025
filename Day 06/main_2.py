05b5223c5cfcd7b79f450437ae10f41427061792
Žiga Pregelj <zpregelj@gmail.com>
2025-12-12 18:46:26 +0100

    day 6 part 2 get whole dataset in list with spaces " "

---
+++ Day 06/main_2.py
@@ -0,0 +1,148 @@
+from cmath import polar
+from sys import exc_info
+
+s = "small_input.txt"
+l = "input.txt"
+
+# new version
+def read_lines(file_name: str) -> tuple[list[str], list[int]]:
+    with open(file_name, "r", encoding="utf-8") as file:
+        lines_to_list = [line.rstrip("\n") for line in file]
+
+        operations = [operation for operation in lines_to_list[-1].strip().split(" ") if operation != ""]
+        numbers_in_columns = []
+        for column in lines_to_list[:-1]:
+            new_column_to_int = [number for number in column]
+            numbers_in_columns.append(column)
+        return operations, numbers_in_columns
+print(read_lines(s))
+
+
+def count_length_of_biggest_number_in_column(file_name: str):
+    with open(file_name, "r", encoding="utf-8") as file:
+        lines_to_list = [line for line in file]
+        count_spaces = 1  # space on last number is missing (no next number) i will add it here 
+        numbers_length_in_column = []
+        # take only last line with operations and reverse it (because is easier to count spaces)
+        for operation in reversed(lines_to_list[-1]):
+            if operation == "*" or operation == "+":
+                numbers_length_in_column.append(count_spaces)
+                count_spaces = 0
+            else:
+                count_spaces += 1
+
+        return numbers_length_in_column[::-1]
+
+
+print(count_length_of_biggest_number_in_column(l))
+print(count_length_of_biggest_number_in_column(s))
+
+
+def cut_string(file_name):
+    operations, numbers_in_columns = read_lines(file_name)
+    cell_length = count_length_of_biggest_number_in_column(file_name)
+    count_numbers_in_row = len(operations)
+    print(count_numbers_in_row)
+    rearranged_numbers = []
+    for line in numbers_in_columns:
+        print(line)
+        new_column =  []
+        main_index = 0
+        # count also " " space between numbers
+        # new_column.append(line[start_index:end_index])
+
+        for i in range(count_numbers_in_row - 1):
+            # print(length)
+            length = cell_length[i]
+            print(length)
+
+            # index_to_add += 1
+            # start_index += index_to_add
+            # end_index += index_to_add
+            new_column.append(line[main_index:main_index + length])
+            main_index = main_index + length + 1
+        
+        # add last number in column
+        new_column.append(line[main_index:main_index + cell_length[-1]])
+        rearranged_numbers.append(new_column)
+        new_column = []
+        start_index = 0
+    return rearranged_numbers
+        
+print(cut_string(l))
+
+# rows to columns
+def columns_into_list(file_name):
+    operations, numbers_in_columns = read_lines(file_name)
+    new_order_column_to_row = []
+    grand_total = 0
+    for i, operation in enumerate(operations):
+        column_to_row = []
+        for j in range(len(numbers_in_columns)):
+            # this is main part to put numbers in right list...
+            column_to_row.append(numbers_in_columns[j][i])
+
+        new_order_column_to_row.append(column_to_row)
+        column_to_row = []
+    return new_order_column_to_row
+
+print(columns_into_list(s))
+
+def index_of_spaces(file_name: str):
+    with open(file_name, "r", encoding="utf-8") as file:
+        lines_to_list = [line.strip() for line in file]
+        numbers_in_row = len([operation for operation in lines_to_list[-1].strip().split(" ") if operation != ""])
+        print(numbers_in_row)
+        i = 0
+        space_index = set()
+        for column in lines_to_list[:-1]:
+            for number in column.strip().split(" "):
+                if number != "":
+                    i += 1
+                else:
+                    space_index.add(i % numbers_in_row)
+        return sorted(space_index)
+
+   
+    
+def not_split(file_name: str):
+    with open(file_name, "r", encoding="utf-8") as file:
+        lines_to_list = [line for line in file]
+        numbers_length_in_column = count_length_of_biggest_number_in_column(file_name)
+        i = 0
+        # print(lines_to_list[-1])
+        for column in lines_to_list[:-1]:
+            for colum_length in numbers_length_in_column:
+                start_index = 0
+                end_index = colum_length
+                # print(column[:3])
+
+
+
+# put columns into one list
+def columns_into_list(file_name):
+    operations, numbers_in_columns = read_lines(file_name)
+    new_order_column_to_row = []
+    grand_total = 0
+    for i, operation in enumerate(operations):
+        column_to_row = []
+        for j in range(len(numbers_in_columns)):
+            # this is main part to put numbers in right list...
+            column_to_row.append(numbers_in_columns[j][i])
+
+        new_order_column_to_row.append("".join(column_to_row))
+        column_to_row = []
+    return new_order_column_to_row
+
+print(columns_into_list(s))
+#this is regex problem... 
+"""
+od 1 do n srevilk
+en presledek prej en potem
+ce je kaj vec presledkov jih moras vzeti k temu...
+
+"""
+
+print(not_split(s))
+print(index_of_spaces(l))
+# print(index_of_spaces(s))
