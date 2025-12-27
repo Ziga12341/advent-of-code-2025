ddffe170ff8ca16d4a0a2b507a1234538135c9a5
Žiga Pregelj <zpregelj@gmail.com>
2025-12-27 11:56:18 +0100

    day 9 part 2 not done yet  - area check functions done

--- Day 09/main.py
+++ Day 09/main.py
@@ -10,6 +10,51 @@ def read_lines(file_name: str) -> list:
 def calculate_rectangle_area(x_1: int, y_1: int, x_2: int, y_2: int):
     return (abs(x_1 - x_2) + 1) * (abs(y_1 - y_2) + 1)
 
+# check it rectangle in one line
+def rectangle_in_one_row(x_1: int, y_1: int, x_2: int, y_2: int):
+    return x_1 == x_2 or y_1 == y_2
+
+# check if any initial red tile more to the outside of particular point in 4 directions
+# first any initial red tile in area right - top from point
+def initial_red_tile_location_top_right(file_name: str, x: int, y: int):
+    red_tile_locations = read_lines(file_name)
+    # if any red tile location is more top and right from input x, y point
+    return any(x_0 >= x and y_0 <= y for x_0, y_0 in red_tile_locations)
+
+print('===')
+print(initial_red_tile_location_top_right(s, 9, 5))
+
+def initial_red_tile_location_bottom_right(file_name:str, x:int, y:int):
+    red_tile_locations = read_lines(file_name)
+    # if any red tile location is more bottom and right from input x, y point
+    return any(x_0 >= x and y_0 >= y for x_0, y_0 in red_tile_locations)
+
+print(initial_red_tile_location_bottom_right(s, 11, 5))
+
+
+def initial_red_tile_location_bottom_left(file_name:str, x:int, y:int):
+    red_tile_locations = read_lines(file_name)
+    # if any red tile location is more bottom and left from input x, y point
+    return any(x_0 <= x and y_0 >= y for x_0, y_0 in red_tile_locations)
+
+print(initial_red_tile_location_bottom_left(s, 2, 7))
+print(initial_red_tile_location_bottom_left(s, 2, 5))
+
+
+def initial_red_tile_location_top_left(file_name:str, x:int, y:int):
+    red_tile_locations = read_lines(file_name)
+    # if any red tile location is more top and left from input x, y point
+    return any(x_0 <= x and y_0 <= y for x_0, y_0 in red_tile_locations)
+
+print(initial_red_tile_location_top_left(s, 2,1))
+print(initial_red_tile_location_top_left(s, 2,3))
+
+# from each rectangle point I will get the opposite points from other corners which do not have red tiles
+# example: if currently i have top left point and bottom right, here i will calculate bottom eft and top right location
+# x1, y1 will become x1, y2 and x2, y2 will become x2, y1
+# then i will check if this new points are still in area of red and green tiles (valid area)
+def mirror_all_rectangle_points(file_name):
+    ...
 
 def compare_each_corner(file_name):
     collect_all_corner_combinations = set()
@@ -21,12 +66,21 @@ def compare_each_corner(file_name):
             if first_corner != second_corner:
                 area_result = calculate_rectangle_area(x_1, y_1, x_2, y_2)
                 if (area_result, second_corner, first_corner) not in collect_all_corner_combinations:
+                    # possible optimization to remove rectangles in one line
+                    # if not rectangle_in_one_row(x_1, y_1, x_2, y_2):
                     collect_all_corner_combinations.add((area_result, first_corner, second_corner))
     return collect_all_corner_combinations
 
 print(sorted(compare_each_corner(s)))
+print((len(compare_each_corner(s))))
+print((len(compare_each_corner(l))))
+
 def part_1(file_name):
     return sorted(compare_each_corner(file_name))[-1][0]
+
+# check if any red tiles is in area more to outside (left and up) of current opposite / mirror point
+
+
 
 
 def part_2(file_name):
@@ -51,3 +105,7 @@ if __name__ == "__main__":
     print(small_input)
     print("First part: ", part_1(l))
     print("Second part: ", part_2(l))
+
+
+# part 2 too low:
+# 93243 - result not correct for case if rectangle is in one line only
