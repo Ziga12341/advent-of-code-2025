7cdaa229bead2f17f2c26a0b634b2e5c7822d8bd
Žiga Pregelj <zpregelj@gmail.com>
2026-01-07 23:38:00 +0100

    day 11 part 1 soled count "out" printed version

--- Day 11/main.py
+++ Day 11/main.py
@@ -1,12 +1,29 @@
+from collections import defaultdict
 s = "small_input.txt"
 l = "input.txt"
 
 
-def read_lines(file_name: str) -> list:
+def read_lines(file_name: str):
     with open(file_name, "r", encoding="utf-8") as file:
-        return [line.strip() for line in file]
+        devices = defaultdict(set)
+        for line in file:
+            split_line = (line.strip().split(": "))
+            from_device = split_line[0]
+            to_device = split_line[1].split(" ")
+            
+            devices[from_device] = set(to_device)
+    return devices
 
+def find_path(file_name, start:str):
+    devices_dictionary = read_lines(file_name)
+    print(devices_dictionary[start])
+    if devices_dictionary[start].__contains__(start):
+        return 1
+    else:
+        for one_path in devices_dictionary[start]:
+            find_path(file_name, one_path)
 
+print(find_path(l, "you"))
 def part_1(file_name):
     ...
 
@@ -16,7 +33,8 @@ def part_2(file_name):
 
 
 def test_part_1():
-    assert part_1(s) == None
+    assert part_1(s) == 5
+    assert part_1(l) == 690
 
 
 def test_part_2():
@@ -25,7 +43,7 @@ def test_part_2():
 
 if __name__ == "__main__":
     small_input = read_lines(s)
-    large_input = read_lines(l)
+    # large_input = read_lines(l)
     print(small_input)
     print("First part: ", part_1(l))
     print("Second part: ", part_2(l))
