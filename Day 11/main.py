46a5391684aef38c71f196e396c558d1983d3ab0
Žiga Pregelj <zpregelj@gmail.com>
2026-01-08 00:01:33 +0100

    day 11 part 1 DONE solved

--- Day 11/main.py
+++ Day 11/main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+
 s = "small_input.txt"
 l = "input.txt"
 
@@ -10,21 +11,18 @@ def read_lines(file_name: str):
             split_line = (line.strip().split(": "))
             from_device = split_line[0]
             to_device = split_line[1].split(" ")
-            
+            # put into a dictionary key from where it comes to set of all possible path form particular one
             devices[from_device] = set(to_device)
     return devices
 
-def find_path(file_name, start:str):
+
+def part_1(file_name, start: str = "you"):
     devices_dictionary = read_lines(file_name)
-    print(devices_dictionary[start])
-    if devices_dictionary[start].__contains__(start):
+    # check if key "out" in set() (return one)
+    if devices_dictionary[start].__contains__("out"):
         return 1
     else:
-        return 1 + sum([find_path(file_name, one_path) for one_path in devices_dictionary[start]])
-
-print(find_path(l, "you"))
-def part_1(file_name):
-    return find_path(file_name, "you") / 3 
+        return sum([part_1(file_name, one_path) for one_path in devices_dictionary[start]])
 
 
 def part_2(file_name):
